eb69117a08ff3ed6c440df9f1a0496f031a58784
BharaniDhanasekar <119785671+BharaniDhanasekar@users.noreply.github.com>
2023-12-30 14:50:07 +0530

    Update ch3_4.c

--- ch3_4.c
+++ ch3_4.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-
-
 int main() {
-    // Declare variables
+    
     int year, day;
     int month=1;
 
-    // Input the year
     printf("Enter the year: ");
     scanf("%d", &year);
 
     day = (1 + ((13 * (month + 1)) / 5) + k + (k / 4) + (j / 4) + 5 * j)% 7;
 
-    // Check if the year is valid (Gregorian calendar starts from 1583)
     if (year >= 1583) {
-
-
-        // Display the result
         printf("\nOn January 1st of %d, the day is: ", year);
 
         switch (day) {
